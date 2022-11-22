5eb64e734d55db010d1f0cd8af0b4e2a498a099e
jaxayM <56200185+jaxayM@users.noreply.github.com>
2022-11-22 21:00:12 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:jaxaym/Blog-AB.git master:gh-pages
+git push -f git@github.com:jaxayM/Blog-AB.git master:gh-pages
 cd -
