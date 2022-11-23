dcdc86c0d1d92c70b4518069ad18861390d7475c
jamay <missour1998@gmail.com>
2022-11-23 02:27:46 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -e
 # build
 yarn run build
 cd dist
-cp index.html 404.html
+#cp index.html 404.html
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:jaxayM/jaxayM.github.io.git master
+git push -f git@github.com:jaxayM/Blog-AB.git master:gh-pages
 cd -
